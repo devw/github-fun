ec2b8f5d110d3f70796507337de049848bc4a9ed
Antonio Pierro <antonio.pierro@gmail.com>
2021-08-30 04:39:04 +0200

    feat: add on and off constant colors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const ON = '#216e39';
+const OFF = 'ebedf0';
 const columns = Array.from(
     document.querySelectorAll('.js-calendar-graph-svg > g > g')
 ).map((e) => Array.from(e.querySelectorAll('rect')));
 
+columns.forEach((col) => {
+    col.forEach((row) => (row.style.fill = ON));
+});
 console.log('columns:', columns);
