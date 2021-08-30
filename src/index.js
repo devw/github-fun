e751d0911ff670705958cd00e067e756dc8754fd
Antonio Pierro <antonio.pierro@gmail.com>
2021-08-30 05:40:39 +0200

    feat: add interpolatorRGB to get RGB color in middle of ON and OFF

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import Color from 'color';
+import interpolateRGB from 'interpolate-rgb';
 
-const ON = '#216e39';
-const OFF = 'ebedf0';
+const OFF = '#216e39';
+const ON = '#ebedf0';
 const columns = Array.from(
     document.querySelectorAll('.js-calendar-graph-svg > g > g')
 ).map((e) => Array.from(e.querySelectorAll('rect')));
 
-columns.forEach((col) => {
-    col.forEach((row) => (row.style.fill = ON));
+const interpolator = interpolateRGB(Color(ON).color, Color(OFF).color);
+
+columns.forEach((col, x) => {
+    col.forEach((rect, y) => {
+        rect.style.fill = Color(interpolator((x + y) / 60)).hex();
+    });
 });
-console.log('columns:', columns);
