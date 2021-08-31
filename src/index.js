adc2579dc013fa187e27455954226ddc5c74dc1c
Antonio Pierro <antonio.pierro@gmail.com>
2021-08-31 18:12:31 +0200

    feat: add offest to scroll the image

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import Color from 'color';
-import interpolateRGB from 'interpolate-rgb';
-
-const OFF = '#216e39';
-const ON = '#ebedf0';
 const columns = Array.from(
     document.querySelectorAll('.js-calendar-graph-svg > g > g')
 ).map((e) => Array.from(e.querySelectorAll('rect')));
 
-const interpolator = interpolateRGB(Color(ON).color, Color(OFF).color);
+const onLoadImage = (img) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    let offset = 0;
+    window.setInterval(() => {
+        columns.forEach((col, x) => {
+            col.forEach((rect, y) => {
+                const pixel = ctx.getImageData(x, y + offset, 1, 1);
+                rect.style.fill = `rgba(${pixel.data.join(',')})`;
+            });
+        });
+        offset = offset > img.height ? 0 : offset + 1;
+    }, 60);
+};
 
-columns.forEach((col, x) => {
-    col.forEach((rect, y) => {
-        rect.style.fill = Color(interpolator((x + y) / 60)).hex();
-    });
-});
+const img = new Image();
+img.src = './image/dog.jpg';
+img.onload = () => onLoadImage(img);
